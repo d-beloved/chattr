1fb93b819cf86fee642d0206299f1982407d55a3
d-beloved <morayodeji@gmail.com>
2020-11-14 02:11:39 +0100

    implement Update username

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,3 +1,14 @@
 (function connect(){
   let socket = io.connect('http://localhost:3000')
+
+  let username = document.querySelector('#username')
+  let usernameBtn = document.querySelector('#usernameBtn')
+  let currentUsername = document.querySelector('.card-header')
+
+  usernameBtn.addEventListener('click', e => {
+    console.log(username.value);
+    socket.emit('change_username', {username: username.value})
+    currentUsername.textContent = username.value;
+    username.value = ''
+  })
 })()
