85f3ef2e34fdf358a5b20b4c0a45c3ef664de3fa
d-beloved <morayodeji@gmail.com>
2020-11-14 01:04:19 +0100

    initialize socket connection

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ app.get('/', (req, res) => {
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`server is running on port ${PORT}`);
 });
+
+// initializing socket.io from the express server connection and then we set up an event
+// using io.on() which will be triggered every time a new connection to the socket gets established.
+const io = socketio(server)
+
+io.on('connection', socket => {
+  console.log("New user connected")
+});
